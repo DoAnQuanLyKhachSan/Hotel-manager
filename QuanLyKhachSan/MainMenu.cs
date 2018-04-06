b55a2b02b29c8f2ad4c80ba67b6f5e47de86e264
danhy989 <danhy989@gmail.com>
2018-04-06 18:08:00 +0700

    update Source

---
+++ QuanLyKhachSan/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using QuanLyKhachSan;
+using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            fRoom _room = new fRoom();
+            _room.ShowDialog();
+            this.Show();
+        }
+    }
+}
