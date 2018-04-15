d964ebbd2d5a8fe6add37b9ffa6982a0841fa4ce
danhy989 <danhy989@gmail.com>
2018-04-15 16:19:17 +0700

    update sourceeeeeee
    Xong hết những thứ trong form ROOM và những form liên quan trong form ROOM . edit lại form thanh toán theo 2 cách xử lý . Cập nhật SQL DATABASE . AE chạy lại Database nhé

---
+++ Hotel-manager-master/QuanLyKhachSan/DAO/PayDAO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.DAO
+{
+    public class PayDAO
+    {
+        private PayDAO() { }
+
+        private static PayDAO instance;
+
+        public static PayDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new PayDAO(); return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        public List<string> getAddressCus(string _name)
+        {
+            List<string> addrList = new List<string>();
+            string query = "EXEC dbo.sp_LayDiaChi @Name ";
+            DataTable data = DataProvide.Instance.ExecuteQuery(query, new object[] { _name });
+            if (data.Rows.Count > 0)
+                foreach (DataRow item in data.Rows)
+                {
+                    addrList.Add(item["DiaChi"].ToString());
+                }
+
+            else
+                addrList = null;
+
+            return addrList;
+        }
+
+        public DataTable LoadPayInfo(string _name, string _diachi)
+        {
+            string query = "EXEC dbo.sp_ChiTietThanhToan @TenKH , @DiaChi";
+            return DataProvide.Instance.ExecuteQuery(query, new object[] { _name, _diachi });
+        }
+
+        public float getTotalPrice(string _name, string _diachi)
+        {
+            float _Price = new float();
+            string query = "EXEC dbo.sp_getTongTien @TenKH , @DiaChi ";
+
+            DataTable data = DataProvide.Instance.ExecuteQuery(query, new object[] {_name, _diachi });
+
+            foreach (DataRow item in data.Rows)
+            {
+                _Price = float.Parse(item["TongTien"].ToString());
+                break;
+            }
+            return _Price;
+        }
+
+    }
+}
---
+++ Hotel-manager-master/QuanLyKhachSan/fPay1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class fPay1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dtgvPayInfo = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txbSumPrice = new System.Windows.Forms.TextBox();
+            this.txbReadAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbReadNameCus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvPayInfo)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 57);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HÓA ĐƠN THANH TOÁN";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtgvPayInfo);
+            this.panel2.Location = new System.Drawing.Point(12, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(781, 242);
+            this.panel2.TabIndex = 5;
+            // 
+            // dtgvPayInfo
+            // 
+            this.dtgvPayInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgvPayInfo.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dtgvPayInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvPayInfo.Location = new System.Drawing.Point(10, 8);
+            this.dtgvPayInfo.Name = "dtgvPayInfo";
+            this.dtgvPayInfo.ReadOnly = true;
+            this.dtgvPayInfo.Size = new System.Drawing.Size(761, 225);
+            this.dtgvPayInfo.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txbSumPrice);
+            this.panel3.Controls.Add(this.txbReadAddress);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txbReadNameCus);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(12, 75);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(781, 98);
+            this.panel3.TabIndex = 2;
+            // 
+            // txbSumPrice
+            // 
+            this.txbSumPrice.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txbSumPrice.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSumPrice.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txbSumPrice.Location = new System.Drawing.Point(528, 63);
+            this.txbSumPrice.Multiline = true;
+            this.txbSumPrice.Name = "txbSumPrice";
+            this.txbSumPrice.ReadOnly = true;
+            this.txbSumPrice.Size = new System.Drawing.Size(243, 32);
+            this.txbSumPrice.TabIndex = 1;
+            // 
+            // txbReadAddress
+            // 
+            this.txbReadAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbReadAddress.Location = new System.Drawing.Point(528, 11);
+            this.txbReadAddress.Multiline = true;
+            this.txbReadAddress.Name = "txbReadAddress";
+            this.txbReadAddress.ReadOnly = true;
+            this.txbReadAddress.Size = new System.Drawing.Size(243, 32);
+            this.txbReadAddress.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(445, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Trị giá :";
+            // 
+            // txbReadNameCus
+            // 
+            this.txbReadNameCus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbReadNameCus.Location = new System.Drawing.Point(190, 11);
+            this.txbReadNameCus.Name = "txbReadNameCus";
+            this.txbReadNameCus.ReadOnly = true;
+            this.txbReadNameCus.Size = new System.Drawing.Size(213, 29);
+            this.txbReadNameCus.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(446, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Địa chỉ :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Khách hàng / Cơ quan : ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(238, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Trở về";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(44, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Xuất hóa đơn";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.button2);
+            this.panel6.Controls.Add(this.button1);
+            this.panel6.Location = new System.Drawing.Point(444, 427);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(349, 59);
+            this.panel6.TabIndex = 0;
+            // 
+            // fPay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 494);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel6);
+            this.Name = "fPay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fPay";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvPayInfo)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dtgvPayInfo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txbSumPrice;
+        private System.Windows.Forms.TextBox txbReadAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbReadNameCus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
