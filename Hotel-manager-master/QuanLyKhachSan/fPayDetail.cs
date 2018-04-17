83257f40bb1b141201282a0703eca421493d1917
NTD <nhokcute533@gmail.com>
2018-04-17 11:02:42 +0700

    update

---
+++ Hotel-manager-master/QuanLyKhachSan/fPayDetail.cs
@@ -0,0 +1,82 @@
+﻿using QuanLyKhachSan.DAO;
+using QuanLyKhachSan.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class fPayDetail : Form
+    {
+        public fPayDetail()
+        {
+            InitializeComponent();
+
+            setTablePayDetail(TablePayDetail);
+
+            LoadPayDetailInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            fPayInfo.getInfo.ListCusPayed = new List<string>();
+            fPayInfo.getInfo.ListRoomPayed = new List<string>();
+            this.Close();
+        }
+
+        DataTable TablePayDetail = new DataTable();
+
+        public void setTablePayDetail(DataTable data)
+        {
+            data.Columns.Add(new DataColumn("MaPhong", typeof(int)));
+            data.Columns.Add(new DataColumn("TenKhachHang", typeof(string)));
+            data.Columns.Add(new DataColumn("SoNgayThue", typeof(int)));
+            data.Columns.Add(new DataColumn("DonGia", typeof(string)));
+            data.Columns.Add(new DataColumn("TongTienPhong", typeof(string)));
+        }
+
+        public void LoadPayDetailInfo()
+        {
+            txbReadNameCus.Text = fPayInfo.getInfo.nameCus;
+            txbReadAddress.Text = fPayInfo.getInfo.addressCus;
+            double totalPrice = 0;
+            if (fPayInfo.getInfo.typePay == 1)
+            {
+                foreach (string item in fPayInfo.getInfo.ListRoomPayed)
+                {
+                    List<PayDTO> list = PayDAO.Instance.getListPayDetailByRoom(int.Parse(item));
+
+                    PayDAO.Instance.ListPayDetailToTable(list, TablePayDetail);
+
+                    totalPrice += PayDAO.Instance.getTotalPrice(item);
+                }
+            }
+            else
+            {
+                foreach (string item in fPayInfo.getInfo.ListCusPayed)
+                {
+                    List<PayDTO> list = PayDAO.Instance.getListPayDetailByCusName(item.ToString());
+
+                    PayDAO.Instance.ListPayDetailToTable(list, TablePayDetail);
+
+                    foreach (PayDTO roomID in list)
+                    {
+                        totalPrice += PayDAO.Instance.getTotalPrice(roomID.RoomID.ToString());
+                    }
+                }
+            }
+
+            dtgvPayDetail.DataSource = TablePayDetail;
+
+            txbTotalPrice.Text = totalPrice.ToString() + "   đ";
+        }
+
+    }
+}
---
+++ Hotel-manager-master/QuanLyKhachSan/fPayInfo.cs
@@ -0,0 +1,140 @@
+﻿using QuanLyKhachSan.DAO;
+using QuanLyKhachSan.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class fPayInfo : Form
+    {
+        public fPayInfo()
+        {
+            InitializeComponent();
+
+            SetPayTable(PayRoomTable, PayCusTable);
+
+        }
+
+        public class getInfo
+        {
+            static public string nameCus;
+            static public string addressCus;
+            static public int typePay;
+            public static List<string> ListRoomPayed = new List<string>();
+            public static List<string> ListCusPayed = new List<string>();
+        }
+
+        DataTable PayRoomTable = new DataTable();
+        DataTable PayCusTable = new DataTable();
+      
+
+        public void SetPayTable(DataTable t1, DataTable t2)
+        {
+            t1.Columns.Add(new DataColumn("MaPhong", typeof(int)));
+            t1.Columns.Add(new DataColumn("NgayBatDauThue", typeof(string)));
+            t1.Columns.Add(new DataColumn("TinhTrangThanhToan", typeof(int)));
+
+            t2.Columns.Add(new DataColumn("MaCTPT", typeof(int)));
+            t2.Columns.Add(new DataColumn("TenKhachHang", typeof(string)));
+            t2.Columns.Add(new DataColumn("MaPhong", typeof(int)));
+            t2.Columns.Add(new DataColumn("NgayBatDauThue", typeof(string)));
+        }
+
+
+        private void cbPayType_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedItem != null)
+            {
+     
+                List<string> list = PayDAO.Instance.getPayData(int.Parse(cb.SelectedIndex.ToString()));
+                cbData.DataSource = list;
+
+            }
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if(cbData.SelectedValue == null)
+            {
+                MessageBox.Show("Chưa chọn dữ liệu để thêm!"); return;
+            }
+
+            if (int.Parse(cbPayType.SelectedIndex.ToString()) == 1)
+            {
+                if (PayDAO.Instance.CompareString(cbData.SelectedValue.ToString(), getInfo.ListRoomPayed))
+                {
+                    MessageBox.Show("Mã phòng này đã được thêm rồi!"); return;
+                }
+                else
+                {
+                    List<PayDTO> listPay = PayDAO.Instance.getRoomPay(int.Parse(cbData.SelectedValue.ToString()));
+
+                    PayDAO.Instance.ListRoomPayToTable(listPay, PayRoomTable);
+
+                    dtgvPay.DataSource = PayRoomTable;
+                    getInfo.ListRoomPayed.Add(cbData.SelectedValue.ToString());
+                }
+            }
+            else
+            {
+                if (PayDAO.Instance.CompareString(cbData.SelectedValue.ToString(), getInfo.ListCusPayed))
+                {
+                    MessageBox.Show("Tên khách hàng này đã được thêm rồi!"); return;
+                }
+
+                else
+                {
+                    List<PayDTO> listPay = PayDAO.Instance.getCusPay(cbData.SelectedValue.ToString());
+
+                    PayDAO.Instance.ListCusPayToTable(listPay, PayCusTable);
+
+                    dtgvPay.DataSource = PayCusTable;
+                    getInfo.ListCusPayed.Add(cbData.SelectedValue.ToString());
+                }
+            }
+        }
+
+        private void btPayDetail_Click(object sender, EventArgs e)
+        {
+
+            if (txtCusName.Text.Length == 0)
+            {
+                MessageBox.Show("Tên khách hàng không được để trống"); return;
+            }
+            if (dtgvPay.DataSource == null)
+            {
+                MessageBox.Show("Chưa thêm dữ liệu để xem thanh toán"); return;
+            }
+
+
+
+            getInfo.typePay = cbPayType.SelectedIndex;
+            getInfo.nameCus = txtCusName.Text;
+            getInfo.addressCus = txtAddress.Text.ToString();
+
+            fPayDetail f = new fPayDetail();
+            this.Hide();
+            f.ShowDialog();
+
+            PayCusTable = new DataTable();
+            PayRoomTable = new DataTable();
+            SetPayTable(PayRoomTable, PayCusTable);
+
+            this.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
