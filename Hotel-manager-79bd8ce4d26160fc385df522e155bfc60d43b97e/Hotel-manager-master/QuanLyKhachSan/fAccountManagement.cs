e571ea62c532a3d0e003015c5de44eca83f9e082
NTD <nhokcute533@gmail.com>
2018-04-22 16:38:53 +0700

    UP

---
+++ Hotel-manager-79bd8ce4d26160fc385df522e155bfc60d43b97e/Hotel-manager-master/QuanLyKhachSan/fAccountManagement.cs
@@ -0,0 +1,264 @@
+﻿using QuanLyKhachSan.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class fAccountManagement : Form
+    {
+        private string GroupCode = null;    // GroupCode và FuctionCode kiểu string vì hạn chế lỗi click vào ô không có dữ liệu của hàm dtgvAccountManagement_CellClick 
+        private string FunctionCode = null;
+        private string userName = null;
+
+        public fAccountManagement()
+        {
+            InitializeComponent();
+        }
+        private int Choose = 0;
+
+        private void LoadUserGroup()
+        {
+            dtgvAccountManagement.DataSource = AccountManagementDAO.Instance.getUserGroupFromDataBase();
+        }
+        private void LoadDecentralication()
+        {
+            dtgvAccountManagement.DataSource = AccountManagementDAO.Instance.getDecentralizationFromDataBase();
+        }
+        private void LoadUsers()
+        {
+            dtgvAccountManagement.DataSource = AccountManagementDAO.Instance.getUsersFromDataBase();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Choose = 1;
+            LoadUserGroup();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Choose = 2;
+            LoadDecentralication();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Choose = 3;
+            LoadUsers();
+        }
+
+        private void dtgvAccountManagement_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+                switch (Choose)
+                {
+                    case 1:
+                        {
+                        GroupCode = dtgvAccountManagement.CurrentRow.Cells["Mã nhóm"].Value.ToString();
+                      
+                        break;
+                    }
+                    case 2:
+                        {
+                            FunctionCode = dtgvAccountManagement.CurrentRow.Cells["Mã chức năng"].Value.ToString();
+                            GroupCode = dtgvAccountManagement.CurrentRow.Cells["Mã nhóm"].Value.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            userName = dtgvAccountManagement.CurrentRow.Cells["Tên đăng nhập"].Value.ToString();
+                            break;
+                        }
+                   
+                }
+        }
+
+        private void button5_Click(object sender, EventArgs e)//button add
+        {
+            try
+            {
+                switch (Choose)
+                {
+                    case 1:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsGroupCode(int.Parse(row.Cells[0].Value.ToString())) == false)
+                                    {
+                                        AccountManagementDAO.Instance.insertGroupUser(int.Parse(row.Cells[0].Value.ToString()), row.Cells[1].Value.ToString());
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsPhanQuyen(int.Parse(row.Cells[0].Value.ToString()), int.Parse(row.Cells[1].Value.ToString())) == false)
+                                    {
+                                        AccountManagementDAO.Instance.insertPhanQuyen(int.Parse(row.Cells[0].Value.ToString()), int.Parse(row.Cells[1].Value.ToString()));
+                                     
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsUser(row.Cells["Tên đăng nhập"].Value.ToString()) == false)
+                                    {
+                                        AccountManagementDAO.Instance.insertUser(row.Cells["Tên đăng nhập"].Value.ToString(), row.Cells["Mật khẩu"].Value.ToString(), int.Parse(row.Cells["Mã nhóm"].Value.ToString()));
+                                      
+                                    }
+                                }
+                            }
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+                MessageBox.Show("Thêm thành công");
+            }
+            catch (Exception ex) { if (ex is SqlException) MessageBox.Show("Nhập sai dữ liệu"); }
+        }
+        private void button6_Click(object sender, EventArgs e)//button delete
+        {
+            try
+            {
+                switch (Choose)
+                {
+                    case 1:
+                        {
+                            int data = DataProvide.Instance.ExecuteNonQuery(AccountManagementDAO.Instance.deleteGroupUser(int.Parse(GroupCode)));
+                            //return data(int) > 0 is success
+                            if (data > 0)
+                            {
+                                MessageBox.Show("Xóa thành công");
+                                LoadUserGroup();
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            int data = DataProvide.Instance.ExecuteNonQuery(AccountManagementDAO.Instance.deletePhanQuyen(int.Parse(GroupCode),int.Parse(FunctionCode)));
+                            //return data(int) > 0 is success
+                            if (data > 0)
+                            {
+                                MessageBox.Show("Xóa thành công");
+                                LoadDecentralication();
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            int data = DataProvide.Instance.ExecuteNonQuery(AccountManagementDAO.Instance.deleteUser(userName));
+                            //return data(int) > 0 is success
+                            if (data > 0)
+                            {
+                                MessageBox.Show("Xóa thành công");
+                                LoadUsers();
+                            }
+                            break;
+                        }
+                    default: break;
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (Choose)
+                {
+                    case 1:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsGroupEdit(int.Parse(row.Cells["Mã nhóm"].Value.ToString()), row.Cells["Tên nhóm"].Value.ToString()) == false)
+                                    {
+                                        AccountManagementDAO.Instance.UpdateGroupUser(int.Parse(row.Cells[0].Value.ToString()), row.Cells[1].Value.ToString(),int.Parse(GroupCode));
+                                        
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsPhanQuyen(int.Parse(row.Cells[0].Value.ToString()), int.Parse(row.Cells[1].Value.ToString())) == false)
+                                    {
+                                        AccountManagementDAO.Instance.UpdatePhanQuyen(int.Parse(row.Cells[0].Value.ToString()), int.Parse(row.Cells[1].Value.ToString()),int.Parse(GroupCode),int.Parse(FunctionCode));
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            foreach (DataGridViewRow row in dtgvAccountManagement.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    if (AccountManagementDAO.Instance.CheckExitsUserEdit(row.Cells["Tên đăng nhập"].Value.ToString(), row.Cells["Mật khẩu"].Value.ToString(),int.Parse(row.Cells["Mã nhóm"].Value.ToString())) == false)
+                                    {
+                                        AccountManagementDAO.Instance.UpdateUser(row.Cells["Tên đăng nhập"].Value.ToString(), row.Cells["Mật khẩu"].Value.ToString(), int.Parse(row.Cells["Mã nhóm"].Value.ToString()),userName);
+                                        
+                                    }
+                                }
+                            }
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+                MessageBox.Show("Sửa thành công");
+            }
+            catch (Exception ex) { if (ex is SqlException)
+                    MessageBox.Show("Sửa thất bại,nên sữa mỗi lần một dòng");
+                switch (Choose)
+                {
+                    case 1: LoadUserGroup();
+                        break;
+                    case 2: LoadDecentralication();
+                        break;
+                    case 3: LoadUsers();
+                        break;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+    }
+}
---
+++ Hotel-manager-79bd8ce4d26160fc385df522e155bfc60d43b97e/Hotel-manager-master/QuanLyKhachSan/fRent.cs
@@ -0,0 +1,126 @@
+﻿using QuanLyKhachSan.DAO;
+using QuanLyKhachSan.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class fRent : Form
+    {
+        CustomerDTO[] _customer = new CustomerDTO[5];
+        fRoom f = new fRoom();
+        MainMenu m = new MainMenu();
+        fViewRoom fview;
+        public fRent(fRoom room, string roomname, MainMenu mainMenu)
+        {
+            InitializeComponent();
+            m = mainMenu;
+            this.txbRoomName.Text = roomname;
+            f = room;
+            loadComboBoxInDataGridView();
+        }
+        public fRent(fViewRoom fv)
+        {
+            InitializeComponent();
+            fview = fv;
+            loadComboBoxInDataGridView();
+        }
+        public fRent(fRoom room)
+        {
+            InitializeComponent();
+            // f = room;
+            loadComboBoxInDataGridView();
+        }
+
+        private int getRoomCode()
+        {
+            return RoomDAO.Instance.ReturnRoomCode(txbRoomName.Text.ToString());
+        }
+
+        private DateTime getBeginDay()
+        {
+            return dtBeginRent.Value.Date.ToLocalTime();
+        }
+
+        public void getInforCustomer()
+        {
+            int i = 0;
+
+            foreach (DataGridViewRow row in dtgvInputCustomes.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    _customer[i] = new CustomerDTO();
+                    _customer[i].CustomerName = row.Cells[0].Value.ToString();
+                    _customer[i].CustomerStyle = RoomDAO.Instance.ReturnCustomerCode(row.Cells[1].Value.ToString());
+                    _customer[i].CustomerCMND = int.Parse(row.Cells[2].Value.ToString());
+                    _customer[i].CustomerAddress = row.Cells[3].Value.ToString();
+                    i++;
+                }
+            }
+        }
+
+        void LoadListRoomAndCloseForm()
+        {
+            f.LoadRoomList();
+            this.Close();
+        }
+
+        private void loadComboBoxInDataGridView()
+        {
+            customerStyle.MaxDropDownItems = 4;
+            customerStyle.ValueMember = "TenLoaiKhachHang";
+            customerStyle.DataSource = DataProvide.Instance.ExecuteQuery("select TenLoaiKhachHang from LOAI_KHACHHANG ");
+        }
+
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (RoomDAO.Instance.checkStatusRoomisRent(getRoomCode()) == true)  // nếu phòng có thể cho thuê thì trả về true
+                {
+                    getInforCustomer();// tạo một danh sách đối tượng khách hàng thuê phòng
+                    RoomDAO.Instance.CreateTenancyCard(getRoomCode(), getBeginDay());//tao phieu thue phong
+                    RoomDAO.Instance.CreateTenancyCardDetail(_customer, getRoomCode(), getBeginDay());//tao chitiet-thuephong
+
+                    MessageBox.Show("Tạo phiếu thuê phòng thành công");
+                    LoadListRoomAndCloseForm();
+                    m.ReLoadRoomStatus();
+                    m.ReLoadStatusOfRooms();
+                    m.AddCustomerToBox(_customer[0].CustomerName);
+                    fViewRoom t = new fViewRoom(m.LoadRoomInfo(getRoomCode()), m.LoadRoomInfor(getRoomCode()), getRoomCode(), m);
+                    fview = new fViewRoom(t);
+                }
+                else   // phòng đang sửa chữa hoặc đang cho thuê
+                {
+                    if (RoomDAO.Instance.checkStatusRoomisRenting(getRoomCode()) == true) MessageBox.Show("Phòng này đang được cho thuê");
+                    else
+                    {
+                        MessageBox.Show("Phòng này đang được sửa chữa");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nhập đúng định dạng : Tên Phòng , CMND kiểu số , Loại khách hàng(nội địa,nước ngoài)");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+}
